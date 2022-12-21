ec32d84c5ff809a98d230eb2bd8f45cfc6ef63fd
mathias weiss <mathiasmichaelweiss@gmail.com>
2022-12-21 10:03:40 +0100

    v 1.0.6

--- src/components/IconCollapse/IconCollapse.tsx
+++ src/components/IconCollapse/IconCollapse.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { IconProps } from "../iconInterfaces";
 
 const IconCollapse = ({ size = 16 }: IconProps) => {
-    const viewBox = `0 0 ${size} ${size}`
+    
 
-    const svg = <svg width={size} height={size} viewBox={viewBox} fill="none" xmlns="http://www.w3.org/2000/svg">
+    const svg = <svg width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M3.31579 6.1579C2.5891 6.1579 2 6.747 2 7.47369V11.6842C2 12.4109 2.5891 13 3.31579 13H12.7895C13.5162 13 14.1053 12.4109 14.1053 11.6842V7.47369C14.1053 6.747 13.5162 6.1579 12.7895 6.1579H3.31579ZM13.0526 11.9474H3.05263V7.21053H13.0526V11.9474Z" fill="#2A2F39"/>
     <path d="M7.26319 8.26315H3.57898V8.78947H7.26319V8.26315Z" fill="#D5DFEB"/>
     <path d="M8.84214 9.3158H3.57898V9.84211H8.84214V9.3158Z" fill="#8692A3"/>
