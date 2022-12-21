e1b4f10f43daf20be78cd090d471e21b3671e39c
mathias weiss <mathiasmichaelweiss@gmail.com>
2022-12-21 11:30:36 +0100

    v 1.0.9

--- src/components/IconBreadcrumbsActive/IconBreadcrumbsActive.tsx
+++ src/components/IconBreadcrumbsActive/IconBreadcrumbsActive.tsx
@@ -4,7 +4,7 @@ import { IconProps } from "../iconInterfaces";
 const IconBreadcrumbsActive = ({ size = 16 }: IconProps) => {
     
 
-    const svg = <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    const svg = <svg width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M6.15827 7.07061C6.3142 7.18756 6.34583 7.40879 6.22886 7.56473L5.4588 8.59146C5.34185 8.7474 5.12062 8.77901 4.96468 8.66205C4.80874 8.5451 4.77714 8.32387 4.8941 8.16794L5.66416 7.1412C5.78112 6.98526 6.00235 6.95365 6.15827 7.07061Z" fill="#2A2F39"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M10.7465 7.07061C10.9025 7.18756 10.9341 7.40879 10.8171 7.56473L10.0471 8.59146C9.93011 8.7474 9.70888 8.77901 9.55295 8.66205C9.39699 8.5451 9.3654 8.32387 9.48236 8.16794L10.2524 7.1412C10.3694 6.98526 10.5906 6.95365 10.7465 7.07061Z" fill="#2A2F39"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M2 7.70588C2 7.51096 2.15803 7.35294 2.35294 7.35294H4.2139C4.40885 7.35294 4.56684 7.51096 4.56684 7.70588V8.02674C4.56684 8.22166 4.40885 8.37968 4.2139 8.37968H2.35294C2.15803 8.37968 2 8.22166 2 8.02674V7.70588Z" fill="#1DBC7B"/>
